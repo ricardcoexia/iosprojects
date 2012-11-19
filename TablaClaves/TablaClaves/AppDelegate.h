a9cc63d55d3d34050c3fece641a01d17d4d77e89
Ricard <rtorres@coexia.cat>
2012-11-19 10:10:56 +0100

    New Project - TablaClaves

---
+++ TablaClaves/TablaClaves/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TablaClaves
+//
+//  Created by Ricard on 19/11/12.
+//  Copyright (c) 2012 Coexia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
