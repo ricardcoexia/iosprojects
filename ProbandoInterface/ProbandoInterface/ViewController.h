4e11c60dfe90385754e2a181c8422dfab322b6d5
Ricard <rtorres@coexia.cat>
2012-11-19 08:19:50 +0100

    New Interface Testing Project

---
+++ ProbandoInterface/ProbandoInterface/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ProbandoInterface
+//
+//  Created by Ricard on 19/11/12.
+//  Copyright (c) 2012 Coexia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
