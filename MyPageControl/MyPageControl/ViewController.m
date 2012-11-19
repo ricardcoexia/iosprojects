3a82cc46c63dcf793e428569afc6cbf556878cd3
Ricard <rtorres@coexia.cat>
2012-11-19 09:43:05 +0100

    Add README

---
+++ MyPageControl/MyPageControl/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MyPageControl
+//
+//  Created by Ricard on 19/11/12.
+//  Copyright (c) 2012 Coexia. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
